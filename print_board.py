e3553cf7bda37594d3ee2c841f2fec169ea7d6a8
Denny Biasiolli <denny.biasiolli@gmail.com>
2016-05-12 12:24:26 +0200

    board: managed frame elapsed from start

--- print_board.py
+++ print_board.py
@@ -9,19 +9,19 @@ class PrintBoardThread(Thread):
         Thread.__init__(self)
         self.refresh_rate_ms = refresh_rate_ms
         self.stringa_comandi = stringa_comandi
-        self.count = 10
         self.board = Board(width, height)
-        self.head_position = Point(width // 2, height // 2)
+        self.head_position = Point(width // 4, height // 2)
+        self.seconds = 0
 
     def run(self):
-        print('Counting down from', self.count)
-        while self.count >= 0:
+        while True:
             os.system('clear')
+            self.board.add_point(self.head_position)
             print(self.board)
-            print('\rCounting down %d' % self.count)
+            print('\rFrames elapsed %d' % self.seconds)
             print('\r%s\r' % self.stringa_comandi)
             print(self.head_position)
-            self.count -= 1
+            self.seconds += 1
             time.sleep(500 / 1000)
         return
 
@@ -32,16 +32,20 @@ class Board(object):
         self.width = width
         self.height = height
         self.__board = []
-        self.__board.append('#' * self.width)
+        self.__board.append(list('#' * self.width))
         for i in range(self.height - 2):
-            self.__board.append('#' + ' ' * (self.width - 2) + '#')
-        self.__board.append('#' * self.width)
+            self.__board.append(list('#' + ' ' * (self.width - 2) + '#'))
+        self.__board.append(list('#' * self.width))
 
     def __repr__(self):
-        ret_val = ''
+        ret_val = '\r'
         for line in self.__board:
-            ret_val += line + '\r\n'
+            ret_val += ''.join(line) + '\r\n'
         return ret_val
+
+    def add_point(self, point):
+        if isinstance(point, Point):
+            self.__board[point.y][point.x] = '*'
 
 class Point(object):
 
