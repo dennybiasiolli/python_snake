3bcc4f79a83c4167c3aecbfb1762a900a199b73e
Denny Biasiolli <denny.biasiolli@gmail.com>
2016-05-12 11:41:41 +0200

    main: added print_board as a thread

--- main.py
+++ main.py
@@ -2,13 +2,14 @@ import sys
 import threading
 
 import input_control
+from print_board import PrintBoardThread
 
 
 k = None
 
 
 def main():
-    field_width = 50
+    field_width = 80
     field_height = 30
     if len(sys.argv) > 1:
         field_width = int(sys.argv[1])
@@ -16,9 +17,12 @@ def main():
             field_height = int(sys.argv[2])
     print('field_width  %d' % field_width)
     print('field_height %d' % field_height)
+    
+    thread = PrintBoardThread(field_width, field_height, 'Input direction or press "q" to quit', 5)
+    thread.daemon = True
+    thread.start()
 
-    while True:
-        print('Input direction or press "q" to quit')
+    while thread:
         k = input_control.get_key()
         if k == '[A':
             print('up')
